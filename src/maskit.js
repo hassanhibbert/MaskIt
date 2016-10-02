1aa3b7233cf3ad32beb6154a05cd242f72cfe96b
H2-CODE\Hassan <hassanhibbert@gmail.com>
2016-10-02 11:24:39 -0400

    update logic for caret position

--- src/maskit.js
+++ src/maskit.js
@@ -19,7 +19,6 @@
     this.maskValue = '';
     this.maskPattern = maskPattern.split('');
     this.maskElement = !isObject(options[0]) && getElementList(options[0]);
-    this.caretState = new Array(3);
 
     this.events = {
       onChangeHandler: _onChangeHandler.bind(_this),
@@ -157,35 +156,39 @@
 
   function _onInputHandler(event) {
     event.preventDefault();
-    var updateCaretPosition = caretPosition.call(this, event.target);
+
+    // Info before caret position is changed
+    var lengthBefore = this.maskValue.length,
+        caretPositionBefore = getCaretPosition(event.target),
+        lengthAfter, caretPosition;
 
     if (this.options.onInputHandler) {
       this.options.onInputHandler.call(null, event.target, this.mask(event.target.value));
     } else {
       event.target.value = this.mask(event.target.value);
-      updateCaretPosition.call(this);
+
+      // Update caret position
+      lengthAfter = event.target.value.length;
+      caretPosition = lengthBefore < lengthAfter ? caretPositionBefore + 1 : caretPositionBefore;
+      setCaretPosition(event.target, caretPosition);
     }
   }
 
-  function caretPosition(selection) {
-    var pos = 0, selectStart = selection.selectionStart, caretState = this.caretState;
-    if (selectStart || selectStart === 0) {
-      pos = selectStart;
-    }
-    return function () {
-      caretState.length >= 3 && caretState.splice(0, 1);
-      caretState.push(selection.value.length);
-      selection.focus();
-
-      if (caretState[0] > caretState[1] && caretState[2] > caretState[1]) {
-        selection.setSelectionRange(pos, pos);
-      } else if (caretState[1] >= caretState[2]) {
-        selection.setSelectionRange(pos, pos);
-      } else if (caretState[1] < caretState[2]) {
-        selection.setSelectionRange(pos + 1, pos + 1);
+  function getCaretPosition(selection) {
+    if (selection.selectionStart) {
+      var pos = 0, selectStart = selection.selectionStart;
+      if (selectStart || selectStart === 0) {
+        pos = selectStart;
       }
+      return pos;
+    }
+  }
 
-    };
+  function setCaretPosition(selection, pos) {
+    if (selection.setSelectionRange) {
+      selection.focus();
+      selection.setSelectionRange(pos, pos);
+    }
   }
 
   // utils
